653fa62063c559dfb8cb4fd60d5ca791bb41cc51
RithikZaveri <64645244+RithikZaveri@users.noreply.github.com>
2021-06-05 21:40:12 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        My Name is Rithik Zaveri. This was an amazing bootcamp taken up by
+        Sharya Sinha. He taught everything from scratch including React and
+        Javascript.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
